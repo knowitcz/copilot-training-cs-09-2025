f4f416441501384023b3c83f1ad013b6b7a6150d
jkoudelka <jiri.koudelka@knowit.cz>
2024-11-26 08:21:59 +0100

    Adding task 8 transaction crud operations implementation for code review

--- src/main/java/cz/knowit/ai/bank/database/transaction/TransactionProdService.java
+++ src/main/java/cz/knowit/ai/bank/database/transaction/TransactionProdService.java
@@ -1,7 +1,10 @@
 package cz.knowit.ai.bank.database.transaction;
 
+import jakarta.annotation.Nonnull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 /**
  * Production implementation of {@link TransactionService}.
@@ -15,12 +18,22 @@ public final class TransactionProdService implements TransactionService {
     private final TransactionRepository transactionRepository;
 
     @Autowired
-    public TransactionProdService(TransactionRepository transactionRepository) {
+    public TransactionProdService(@Nonnull TransactionRepository transactionRepository) {
         this.transactionRepository = transactionRepository;
     }
 
     @Override
     public void save(Transaction transaction) {
         transactionRepository.save(transaction);
+    }
+
+    @Override
+    public Optional<Transaction> getById(long id) {
+        return transactionRepository.findById(id);
+    }
+
+    @Override
+    public void deleteById(long id) {
+        //TODO: Implement
     }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,18 @@
 INSERT INTO T_CLIENT (id, first_name, last_name, age, phone_number, nationality)
     VALUES (-1, 'John', 'Doe', 25, '+420111222333', 'CZECH_REPUBLIC');
 
-INSERT INTO T_ACCOUNT (id, name, balance, type, client_id) VALUES (-1, 'John', 1000, 'CASH', -1);
+INSERT INTO T_ACCOUNT (id, name, balance, type, client_id)
+    VALUES (-1, 'John', 1000, 'CASH', -1);
+
+INSERT INTO T_TRANSACTION (id, amount, created_at, account_id)
+    VALUES (-1, 100, '2020-01-01 00:00:00', -1);
+INSERT INTO T_TRANSACTION (id, amount, created_at, account_id)
+    VALUES (-2, 200, '2020-02-01 00:00:00', -2);
+INSERT INTO T_TRANSACTION (id, amount, created_at, account_id)
+    VALUES (-3, 300, '2020-03-01 00:00:00', -1);
+INSERT INTO T_TRANSACTION (id, amount, created_at, account_id)
+    VALUES (-4, 400, '2020-04-01 00:00:00', -1);
+INSERT INTO T_TRANSACTION (id, amount, created_at, account_id)
+    VALUES (-5, 500, '2020-05-01 00:00:00', -1);
+INSERT INTO T_TRANSACTION (id, amount, created_at, account_id)
+    VALUES (-6, 600, '2020-06-01 00:00:00', -1);
